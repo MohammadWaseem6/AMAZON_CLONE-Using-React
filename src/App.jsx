4797acce2399d3a1cd943fd363d74b82598ef08c
MohammadWaseem6 <wa6865688@gmail.com>
2024-03-09 12:10:59 +0530

    First commit (Amazon clone stared to build)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+
+function App() {
+    return (
+
+        <BrowserRouter>
+            <Routes>
+            <Route exact path="/" element={<HomePage />} />
+
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
